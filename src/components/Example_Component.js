0b6e8549a365ef4906229a1da9a70c670c016629
Jose Hernandez <lovingmobo@Joses-MacBook-Pro.local>
2023-06-23 14:42:04 -0500

    brain storming ideas

--- src/components/Example_Component.js
+++ src/components/Example_Component.js
@@ -1,10 +1,28 @@
-import React, {  } from 'react'; //useContext
-// import { AppContext } from '../context/AppContext';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
 const Example_component = () => {
+    const { dispatch, currentTheme, cssValues, updateCssValues } = useContext(AppContext);
+
+    const changeTheme = (e) => {
+
+        updateCssValues({ backgroundColor: 'blue' });
+
+        console.log(currentTheme)
+        dispatch({
+            type: 'CHANGE_THEME',
+            payload: e.target.value,
+        });
+        console.log(currentTheme)
+    }
 
     return (
-        <div>
-            <span>This is where i left off for tonight!</span>
+        <div style={cssValues}>
+
+            <select onChange={changeTheme}>
+                <option value="fullStackTheme">Full Stack Theme</option>
+                <option value="personalTheme">Personal Theme</option>
+            </select>
         </div>
     );
 };
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,22 +1,27 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useState } from 'react';
 
 
 //See if you can find a story telling API ;) 7*-----
 
 
-export const AppReducer = (/*state, action*/) => {
+export const AppReducer = (state, action) => {
 
     //*-- We could have a switch here -- *//
 
-    // switch(action.type) {
+    switch(action.type) {
+        case 'CHANGE_THEME': //2*--
 
+            console.log("Switching to: " + action.payload);
+            state.currentTheme = action.payload;
+            action.type = "DONE";
+            return state;
 
-    //     case 'EXAMPLE-1': //2*--
+        default:
+            return {
+                ...state,
+            };
 
-    //     default:
-    //         return state;
-
-    // }
+    }
 
 
 
@@ -32,6 +37,9 @@ const initialState = {
     //*This can be basically anything you want it to be.*//
 
     //Add all your component states here. It will make everything so much easier! 8*-----
+
+    currentTheme: 'fullStackTheme',
+
 };
 
 export const AppContext = createContext();
@@ -42,11 +50,32 @@ export const AppContext = createContext();
 export const AppProvider = (props) => {
     // 4. Sets up the app state. takes a reducer, and an initial state
     const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    const [cssValues, setCssValues] = useState({
+        backgroundColor: 'red',
+        fontSize: '16px',
+        height: 'fit-content',
+        width: 'fit-content',
+        padding: '1rem',
+        backgroundColor: 'antiquewhite',
+      })
+    
+      const updateCssValues = (newValues) => {
+        setCssValues((prevValues) => ({
+          ...prevValues,
+          ...newValues,
+        }));
+      };
     return (
         <AppContext.Provider
             value={{
                 //* This can be basically anything as well*// 6*------ Don't forget this!
+                dispatch,
+                currentTheme: state.currentTheme,
+                cssValues, 
+                updateCssValues
             }}
+
         >
             {props.children}
         </AppContext.Provider>
