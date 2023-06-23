7f3c17157ba8a77382a6fca8b82796b1eaf42b62
Jose Hernandez <lovingmobo@Joses-MacBook-Pro.local>
2023-06-22 21:13:19 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,29 +4,23 @@ import React from 'react';
 
 import './App.css';
 
+import Example_component from './components/Example_Component';
 
 // import EXMAPLE_COMPONENT_1 from './components/EXMAPLE_COMPONENT_1'; 4*--  This are where all your components should reside
 // Add code to import the other components here under
 import { AppProvider } from './context/AppContext';
 console.log("What's up dude! We're re-learning React!");
 console.log("Week 1 of the rest of my life!");
-console.log(`Come back to this and figure out how this narrative could be played out in real time for every instance
-the page opens. The purpose is to grip the developer into a journey that unfolds as I am living it.
+console.log(`Come back to this and figure out how this narrative could be played out in real time for every instancethe page opens. The purpose is to grip the developer into a journey that unfolds as I am living it.Come join me on my journey to the stars as we discover a new path foward.`);
 
-Come join me on my journey to the stars as we discover a new path foward.`);
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-  
-      </header>
-
       <AppProvider>
           <div className='container'>
               {/* This is where you will be structuring the HTML and CSS. Within JS. In the form of components! 5*-----  */} 
+              <Example_component />
           </div>    
       </AppProvider>
-    </div>
   );
 };
 
---
+++ src/components/Example_Component.js
@@ -0,0 +1,12 @@
+import React, {  } from 'react'; //useContext
+// import { AppContext } from '../context/AppContext';
+const Example_component = () => {
+
+    return (
+        <div>
+            <span>This is where i left off for tonight!</span>
+        </div>
+    );
+};
+
+export default Example_component;
