88175a596813637483ae67a7f2aebf196b81555c
Jose Hernandez <jmhern25yahoo.com>
2023-07-21 12:36:04 -0500

    Added the demo mesh and canvas. Going to be experimenting and learning more about this library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
 import Portfolio from './components/Portfolio';
 import { AppProvider } from './context/AppContext';
 
+import './App.css';
+
+
+function Box(props) {
+    // This reference will give us direct access to the mesh
+    const meshRef = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    // Return view, these are regular three.js elements expressed in JSX
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1.5 : 1}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
+
 const App = () => {
-  return (
-      <AppProvider>
-          <div className='container'>
-              <Portfolio />
-          </div>    
-      </AppProvider>
-  );
+    return (
+        <AppProvider>
+            <div className='backgroundThreeJS'>
+                <Canvas>
+                    <ambientLight />
+                    <pointLight position={[10, 10, 10]} />
+                    <Box position={[-1.2, 0, 0]} />
+                    <Box position={[1.2, 0, 0]} />
+                </Canvas>
+            </div>
+            <div className='container'>
+                <Portfolio />
+            </div>
+        </AppProvider>
+    );
 };
 
 
